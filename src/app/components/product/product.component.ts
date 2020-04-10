4711c34ad4b952b05f2cd45babaefc88d7eee70c
Visaka <visaka.chaves@consorcio.cl>
2020-04-10 16:40:35 -0400

    Ciclo de vida de los componentes

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  EventEmitter,
+  Output,
+  OnChanges,
+  OnDestroy,
+  DoCheck,
+  SimpleChanges
+} from '@angular/core';
 import { MaterialModule } from '../../material/material.module';
 import { Product } from '../../product.model';
 
@@ -7,15 +17,30 @@ import { Product } from '../../product.model';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
   @Input() product: Product;
   // EVent emitter
   @Output() productClick: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  constructor() {
+    console.log('COnstructor product')
+   }
 
+  // ngOnChanges(changes: SimpleChanges) {
+  //   console.log(`${this.product.id} on changes`)
+  //   console.log(changes)
+  // }
   ngOnInit(): void {
+    console.log(`${this.product.id} on Init`)
   }
+  ngDoCheck() {
+    console.log(`${this.product.id} DoCheck`)
+  //   console.log(changes)
+  }
+  ngOnDestroy() {
+    console.log(`${this.product.id} OnDestroy`)
+  }
+
   add() {
     console.log('Add carrito')
     this.productClick.emit(this.product.id)
