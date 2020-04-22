6e6d76d165b788b8072dee0fa90b711b246f967f
Visaka Devi <visaka.chaves@consorcio.cl>
2020-04-22 08:42:17 -0400

    Usa un pipe para hacer un contador de productos

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+/* Pipes */
+import { map } from 'rxjs/operators';
+
 import { CartService } from 'src/app/core/services/cart/cart.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,15 +11,29 @@ import { CartService } from 'src/app/core/services/cart/cart.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  total: Number = 0;
+  /*  total: Number = 0; */
+  /* Observable */
+  total$: Observable<number>
   constructor(
     private cartService: CartService,
   ) {
-    this.cartService.cart$.subscribe(products => {
+    /* this.cartService.cart$.subscribe(products => {
       console.log(products)
       this.total = products.length;
-    })
-   }
+    }) */
+    /* Mejor Practica */
+    /*     this.cartService.cart$
+          .pipe(
+            map(products => this.total = products.length)
+          )
+          .subscribe(total => this.total = total)
+       } */
+    /* Mas Elegante */
+    this.total$ = this.cartService.cart$
+      .pipe(
+        map(products => products.length)
+      )
+  }
 
   ngOnInit(): void {
   }
